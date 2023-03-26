8e24b74f4744ab4658a201041e9fc9106fccefd6
Vipin-Kumar-N <binduvipinkumar39@gmail.com>
2023-03-26 17:24:18 +0530

    Integrated loctions

--- HousePricePredictor/ProjectFiles/UI/main.py
+++ HousePricePredictor/ProjectFiles/UI/main.py
@@ -1,13 +1,15 @@
 # flask, scikit-learn, pandas, pickle-mixin
-
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+data = pd.read_csv('../Csv/Cleaned_Data.csv')
 
 @app.route('/')
 def index():
-    return render_template('Index.html')
+
+    locations = sorted(data['location'].unique())
+    return render_template('Index.html', locations=locations)
 
 
 if __name__ == "__main__":

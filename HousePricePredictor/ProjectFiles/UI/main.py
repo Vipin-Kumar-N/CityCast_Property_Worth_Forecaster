9bafe5623b66c02e05dc97507ceb823f5894be50
Vipin-Kumar-N <binduvipinkumar39@gmail.com>
2023-03-26 16:46:57 +0530

    Basic index page done and hosted using flask Sucessfully

--- HousePricePredictor/ProjectFiles/UI/main.py
+++ HousePricePredictor/ProjectFiles/UI/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+#flask, scikit-learn, pandas, pickle-mixin
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, request
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/')
+def index():
+    return render_template('Index.html')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)

1720ff1f654128867db5ac1faddfc03946627621
Vipin-Kumar-N <binduvipinkumar39@gmail.com>
2023-03-27 22:52:15 +0530

    Defined predict function also modified index.html project working fine with minimal feature

--- HousePricePredictor/ProjectFiles/UI/main.py
+++ HousePricePredictor/ProjectFiles/UI/main.py
@@ -1,19 +1,31 @@
 # flask, scikit-learn, pandas, pickle-mixin
 import pandas as pd
 from flask import Flask, render_template, request
-
+import pickle
+import numpy as np
 app = Flask(__name__)
 data = pd.read_csv('../Csv/Cleaned_Data.csv')
+pipe = pickle.load(open("../Pickle/RidgeModel.pkl", 'rb'))
+
 
 @app.route('/')
 def index():
-
     locations = sorted(data['location'].unique())
     return render_template('Index.html', locations=locations)
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    return
+    locations = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    totalsqft = request.form.get('sqft')
+
+    print(locations, bhk, bath, totalsqft)
+    dat = pd.DataFrame([[locations, totalsqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(dat)[0] * 1e5
+
+    return str(np.round(prediction,2))
 
 
 if __name__ == "__main__":

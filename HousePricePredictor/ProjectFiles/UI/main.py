57659ad7b2219092804300f0f341b1c764506d99
Vipin-Kumar-N <binduvipinkumar39@gmail.com>
2023-04-07 16:23:53 +0530

    Model Working Smoothly

--- HousePricePredictor/ProjectFiles/UI/main.py
+++ HousePricePredictor/ProjectFiles/UI/main.py
@@ -5,10 +5,11 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
+
+data = pd.read_csv('../Csv/Pune_Cleaned_Data.csv')
+pipe = pickle.load(open("../Pickle/PuneRidgeModel.pkl", 'rb'))
 bangaloreData = pd.read_csv('../Csv/Cleaned_Data.csv')
 bangalorePipe = pickle.load(open("../Pickle/RidgeModel.pkl", 'rb'))
-puneData = pd.read_csv('../Csv/Pune_House_Data.csv')
-punePipe = pickle.load(open("../Pickle/PuneRidgeModel.pkl", 'rb'))
 
 
 @app.route('/')
@@ -16,16 +17,30 @@ def index():
     return render_template('Index.html')
 
 
+@app.route('/pune')
+def pune():
+    locations = sorted(data['location'].unique())
+    return render_template('pune.html', locations=locations)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    locations = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    totalsqft = request.form.get('sqft')
+
+    print(locations, bhk, bath, totalsqft)
+    dat = pd.DataFrame([[locations, totalsqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(dat)[0] * 1e5
+
+    return str(np.round(prediction, 2))
+
+
 @app.route('/bangalore')
 def bangalore():
     bangalore_locations = sorted(bangaloreData['location'].unique())
     return render_template('Bangalore.html', locations=bangalore_locations)
-
-
-@app.route('/pune')
-def pune():
-    pune_locations = sorted(puneData['location'].unique())
-    return render_template('pune.html', punelocations=pune_locations)
 
 
 @app.route('/bangalore_predict', methods=['POST'])
@@ -40,20 +55,6 @@ def bangalore_predict():
     bangalore_prediction = bangalorePipe.predict(dat)[0] * 1e5
 
     return str(np.round(bangalore_prediction, 2))
-
-
-@app.route('/punePredict', methods=['POST'])
-def pune_predict():
-    locations = request.form.get('location')
-    bhk = request.form.get('bhk')
-    bath = request.form.get('bath')
-    totalsqft = request.form.get('sqft')
-
-    print(locations, bhk, bath, totalsqft)
-    dat = pd.DataFrame([[locations, totalsqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
-    pune_prediction = punePipe.predict(dat)[0] * 1e5
-
-    return str(np.round(pune_prediction, 2))
 
 
 if __name__ == "__main__":
